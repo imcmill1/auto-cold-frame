5d094a03c1f7820273942ad70a61f1566a800013
Ian <ian.mcmillan1@valpo.edu>
2022-12-06 11:17:02 -0600

    Fixing branch to match main
    Added Critical_Subassembly folder and sketch file to match main. 
    
    //ismc

---
+++ Critical_Subassembly/Critical_Subassembly.ino
@@ -0,0 +1,18 @@
+/* Aidan Mizwicki & Ian McMillan *\
+/* Critical Subassembly Code     *\
+/*********************************\
+/*********************************\
+/* The goal is to have the stuff *\
+/* working                       *\
+/*********************************\
+ 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
